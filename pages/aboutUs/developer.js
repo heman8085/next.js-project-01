8eb4326d90643e2b12d9e6470657fe8458747178
himanshu shukla <hemi8085@gmail.com>
2024-07-18 07:00:23 +0530

    Making Nested Routes

---
+++ pages/aboutUs/developer.js
@@ -0,0 +1,9 @@
+function Developer(){
+    return (
+        <>
+            <h1>Himanshu Shukla </h1> 
+            <h3>    a full stack developer at Sharpener</h3>
+        </>
+    )
+}
+export default Developer;
